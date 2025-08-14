b0a835dd729b22e7be84b1b2086fb8472d438b4c
yeraylois <yeray.lois@udc.es>
2025-08-14 02:40:54 +0200

    feat(PowerMonitor): add C++ library for optocoupler-based power monitoring

---
+++ src/power/PowerMonitor.cpp
@@ -0,0 +1,88 @@
+/**************************************************************
+ *   Project : Blackout Traffic Light System                  *
+ *   Author  : Yeray Lois Sanchez                             *
+ *   Email   : yerayloissanchez@gmail.com                     *
+ **************************************************************/
+
+#include "PowerMonitor.h"
+
+static uint8_t   _pinInput;
+static uint8_t   _pinLed;
+static bool      _invert        = false;
+static uint16_t  _debounce      = 0;
+static bool      _lastStatus    = false;
+static uint32_t  _lastChangeMs  = 0;
+static void    (*_onChangeCb)(bool) = nullptr;
+
+/**
+ * INITIALIZE THE MONITOR: SETUP INPUT AND LED PINS
+ */
+void PM_init(uint8_t inputPin, uint8_t ledPin, bool inputPullup) {
+  _pinInput = inputPin;
+  _pinLed   = ledPin;
+  pinMode(_pinInput, inputPullup ? INPUT_PULLUP : INPUT);
+  pinMode(_pinLed,   OUTPUT);
+  digitalWrite(_pinLed, LOW);
+}
+
+/**
+ * READ RAW DIGITAL VALUE FROM OPTOCOUPLER PIN
+ */
+bool PM_readRaw() {
+  bool val = digitalRead(_pinInput) == HIGH;
+  return _invert ? !val : val;
+}
+
+/**
+ * RETURN TRUE IF POWER IS OK (RAW == HIGH, AFTER INVERSION AND DEBOUNCE)
+ */
+bool PM_isPowerOk() {
+  bool current = PM_readRaw();
+  uint32_t now = millis();
+
+  if (_debounce > 0) {
+    if (current != _lastStatus && (now - _lastChangeMs) < _debounce) {
+      // STILL WITHIN DEBOUNCE WINDOW, IGNORE CHANGE
+      return _lastStatus;
+    }
+    if (current != _lastStatus) {
+      _lastStatus   = current;
+      _lastChangeMs = now;
+      if (_onChangeCb) _onChangeCb(current);
+    }
+  } else {
+    if (current != _lastStatus) {
+      _lastStatus = current;
+      if (_onChangeCb) _onChangeCb(current);
+    }
+  }
+  return _lastStatus;
+}
+
+/**
+ * UPDATE LED INDICATOR ACCORDING TO POWER STATUS
+ */
+void PM_updateLED() {
+  digitalWrite(_pinLed, PM_isPowerOk() ? LOW : HIGH);
+}
+
+/**
+ * ATTACH A CALLBACK TO BE CALLED ON STATUS CHANGE
+ */
+void PM_onChange(void (*callback)(bool)) {
+  _onChangeCb = callback;
+}
+
+/**
+ * INVERT LOGIC (IF OPTO IS ACTIVE-LOW)
+ */
+void PM_invertLogic(bool invert) {
+  _invert = invert;
+}
+
+/**
+ * DEBOUNCE READINGS: SET DEBOUNCE TIME IN MILLISECONDS
+ */
+void PM_setDebounce(uint16_t ms) {
+  _debounce = ms;
+}
